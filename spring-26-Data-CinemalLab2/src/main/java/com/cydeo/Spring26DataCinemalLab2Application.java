50f4f13a7e9ff12cb831e953e4e2d4fac04a32d2
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-05-08 11:24:07 -0400

    MySpring Training Full course

---
+++ spring-26-Data-CinemalLab2/src/main/java/com/cydeo/Spring26DataCinemalLab2Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring26DataCinemalLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring26DataCinemalLab2Application.class, args);
+	}
+
+}
