957ef3177848656baddbeb8535e6be070b0be204
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-05-09 21:07:14 -0400

    MySpring Training Full course

--- spring-26-Data-CinemalLab2/src/main/java/com/cydeo/Spring26DataCinemalLab2Application.java
+++ spring-26-Data-CinemalLab2/src/main/java/com/cydeo/Spring26DataCinemalLab2Application.java
@@ -1,13 +1,22 @@
 package com.cydeo;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Spring26DataCinemalLab2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring26DataCinemalLab2Application.class, args);
+	}
+
+
+	public MigrateResult migrateResult(DataSource dataSource){
+		return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
 	}
 
 }
--- spring-26-Data-CinemalLab2/src/main/java/com/cydeo/entity/Movie.java
+++ spring-26-Data-CinemalLab2/src/main/java/com/cydeo/entity/Movie.java
@@ -3,15 +3,13 @@ package com.cydeo.entity;
 
 import com.cydeo.enums.MovieState;
 import com.cydeo.enums.MovieType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
@@ -29,4 +27,10 @@ public class Movie extends  BaseEntity{
     @Enumerated(EnumType.STRING)
     private MovieState state;
     private BigDecimal price;
+
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))                        // by using annotation @JoinTable we are modify third table created by Spring / join table
+    private List<Genre> genreList;
 }
