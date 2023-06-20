7f8ca93705c0f49a45170db47aee899afb8670cf
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-20 13:29:52 -0400

    MySpring Training Full course

--- spring-21-AOP/src/main/java/com/cydeo/annotation/LoggingAnnotation.java
+++ spring-21-AOP/src/main/java/com/cydeo/annotation/LoggingAnnotation.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD}) // we can put our annotation above method - method level
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoggingAnnotation {
 }
