c648f3638e4821b6a2d515fedb8d2d8a816cdfa0
Boban Milosevic <bobikop@gmail.com>
2023-11-15 21:35:40 -0500

    update

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/repository/DBCommentRepository.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/repository/DBCommentRepository.java
@@ -8,6 +8,7 @@ public class DBCommentRepository implements CommentRepository{
 
     @Override
     public void storeComment(Comment comment) {
+
         System.out.println("Storing comment:" + comment.getText());
     }
 }
